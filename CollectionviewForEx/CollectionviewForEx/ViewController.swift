dfbcaa9563b6acbaed903b885df904fb05598c0e
massOan <backflow00@gmail.com>
2023-09-21 00:19:11 +0900

    commit
    CollectionView delegate 적용

--- CollectionviewForEx/CollectionviewForEx/ViewController.swift
+++ CollectionviewForEx/CollectionviewForEx/ViewController.swift
@@ -2,18 +2,39 @@
 //  ViewController.swift
 //  CollectionviewForEx
 //
-//  Created by 장익준 on 2023/09/20.
+//  Created by  on 2023/09/20.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    let dataList = Reminder.sampleData
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        collectionView.register(UINib(nibName: "RemindCollectionViewCell", bundle: .main), forCellWithReuseIdentifier: "RemindCollectionViewCell")
+
+        
     }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        dataList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RemindCollectionViewCell", for: indexPath) as? RemindCollectionViewCell else {
+            return UICollectionViewCell()
+
+        }
+            return cell
+    }
+    
+    
 
 }
 
