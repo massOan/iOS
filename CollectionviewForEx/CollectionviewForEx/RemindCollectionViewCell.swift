a7a78a7f8933e1f60bb071ced5c43742870aa754
massOan <backflow00@gmail.com>
2023-09-21 00:15:17 +0900

    add
    RemindCollectionViewCell Xib file add

---
+++ CollectionviewForEx/CollectionviewForEx/RemindCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  RemindCollectionViewCell.swift
+//  CollectionviewForEx
+//
+//  Created by 장익준 on 2023/09/20.
+//
+
+import UIKit
+
+class RemindCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
