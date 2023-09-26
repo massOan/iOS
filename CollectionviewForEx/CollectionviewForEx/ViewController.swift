6117742e7de5e787b33c357b711a26b787963d8b
시영 <bartimore00@naver.com>
2023-09-26 16:49:28 +0900

    commit
    Header,Footer commit

--- CollectionviewForEx/CollectionviewForEx/ViewController.swift
+++ CollectionviewForEx/CollectionviewForEx/ViewController.swift
@@ -9,15 +9,23 @@ class ViewController: UIViewController{
     
     @IBOutlet weak var collectionView: UICollectionView!
     
+    fileprivate let headerId = "headerId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
         collectionView.backgroundColor = .systemBackground
+        
         collectionView.register(UINib(nibName: "RemindCollectionViewCell", bundle: .main), forCellWithReuseIdentifier: "RemindCollectionViewCell")
         
-        collectionView.register(HeaderCollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: HeaderCollectionReusableView.identifier)
-
+        collectionView.register(UINib(nibName: String(describing: CustomHeaderView.self), bundle: .main),
+                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                                withReuseIdentifier: String(describing: CustomHeaderView.self))
+        collectionView.register(UINib(nibName: String(describing: CustomFooterView.self), bundle: .main),
+                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter,
+                                withReuseIdentifier: String(describing: CustomFooterView.self))
+        
         func reminderCellRegistration() ->
         UICollectionView.CellRegistration<UICollectionViewListCell, Reminder > {
             return .init { cell, indexpath, item in
@@ -35,12 +43,21 @@ class ViewController: UIViewController{
     }
     
 }
-
-extension ViewController : UICollectionViewDelegate, UICollectionViewDataSource{
+extension ViewController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: 200, height: 200)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: 200, height: 200)
+    }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         Reminder.sampleData.count
     }
+    
+    
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RemindCollectionViewCell", for: indexPath) as! RemindCollectionViewCell
@@ -54,18 +71,24 @@ extension ViewController : UICollectionViewDelegate, UICollectionViewDataSource{
         cell.TodoDescription.sizeToFit()
         
         cell.layer.cornerRadius = 8
-        //
+        
         return cell
-    }
+    }    
     
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: HeaderCollectionReusableView.identifier, for: indexPath) as! HeaderCollectionReusableView
-        
-        return header
-    }
- 
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout : UICollectionViewLayout, referenceSizeForHeaderInSection section : Int) -> CGSize {
-        return CGSize(width: view.frame.size.width, height: 200)
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            return collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                                   withReuseIdentifier: String(describing: CustomHeaderView.self),
+                                                                   for: indexPath) as? CustomHeaderView ?? UICollectionReusableView()
+        case UICollectionView.elementKindSectionFooter:
+            return collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                                   withReuseIdentifier: String(describing: CustomFooterView.self),
+                                                                   for: indexPath) as? CustomFooterView ?? UICollectionReusableView()
+        default:
+            return UICollectionReusableView()
+        }
     }
     
 }
+
