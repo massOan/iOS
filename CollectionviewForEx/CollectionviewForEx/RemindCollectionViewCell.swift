9b21b3a4b6e243c9dd8692ceac0c7d40c1f1c460
시영 <bartimore00@naver.com>
2023-10-02 17:08:58 +0900

    commit
    Cell imageButton scale Type 변경 및 Constraint 조건 변경

--- CollectionviewForEx/CollectionviewForEx/RemindCollectionViewCell.swift
+++ CollectionviewForEx/CollectionviewForEx/RemindCollectionViewCell.swift
@@ -8,6 +8,7 @@ import UIKit
 
 class RemindCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var CompleteButton: UIButton!
     @IBOutlet weak var TodoDescription: UILabel!
     @IBOutlet weak var TodoTitle: UILabel!
     override func awakeFromNib() {
