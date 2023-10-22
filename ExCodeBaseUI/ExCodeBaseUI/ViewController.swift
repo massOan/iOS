5b9789379e729126f35b565d3876f6b62ad4cb9d
시영 <bartimore00@naver.com>
2023-10-22 22:44:20 +0900

    commit
    Codebase 환경셋팅 및 Button 실습

--- ExCodeBaseUI/ExCodeBaseUI/ViewController.swift
+++ ExCodeBaseUI/ExCodeBaseUI/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var nextButton = UIButton()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setUI()
+        nextButton = setUpButton()
     }
+    
+    func setUpButton() {
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        nextButton.configuration? = .filled()
+        nextButton.configuration?.title = "Next"
+        
+        NSLayoutConstraint.activate([
+            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nextButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            nextButton.widthAnchor.constraint(equalToConstant: 200)
+             
+        ])
+    
+        
+    }
+    
+    
+    
+    private func setUI() {
+    let  backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+         backgroundImage.backgroundColor = .green
+         backgroundImage.contentMode = .scaleAspectFill
+//       self.view.insertSubview(backgroundImage, at: 0)
+         self.view.addSubview(backgroundImage)
+        
+        
+        //addSubView, InsertSubView의 차이
+        
+    }
+    
+ }
 
-
-}
 
